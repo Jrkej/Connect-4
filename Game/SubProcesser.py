20d6b649adbcdc1da3e1c899f2f22c88badced87
Aryan Gupta <71920399+aryan-gupta7@users.noreply.github.com>
2021-09-30 21:30:00 +0530

    Update SubProcesser.py

--- Game/SubProcesser.py
+++ Game/SubProcesser.py
@@ -1,8 +1,10 @@
+# Importing necessary modules to run the bot playing the game.
 from subprocess import Popen, PIPE
 from .constants import const
 
 cs = const()
 
+# Executing the bot executable file.
 def MASTER(id = 1, board = ("0"*42)):
     AI = Popen([cs.directory + '\\Master.exe'], shell=True, stdout=PIPE, stdin=PIPE)
     AI.stdin.write(bytes(f'{id}\n', 'UTF-8'))
